dcc6a28f0f67ea96e18e1b32e5695f9678fd4433
nader <clashnader8@gmail.com>
2026-09-20 12:05:51 +0300

    configration a file Strutre

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Primary Islamic Emerald / Forest Greens
+  static const Color primary = Color(0xFF23422A);
+  static const Color primaryLight = Color(0xFF3A5A40);
+  static const Color primaryDark = Color(0xFF142419);
+  static const Color sageGreen = Color(0xFFACD0AF);
+  static const Color mintGreen = Color(0xFFC1EEBC);
+
+  // Warm Spiritual Gold / Amber Accents
+  static const Color gold = Color(0xFFC8963E);
+  static const Color goldLight = Color(0xFFF4BD61);
+  static const Color goldDark = Color(0xFF7E5700);
+  static const Color goldAccent = Color(0xFFFDC668);
+
+  // Light Theme Colors (ورق المصحف الكريمي الدافئ)
+  static const Color lightBackground = Color(0xFFFAF9F4);
+  static const Color lightSurface = Color(0xFFFFFFFF);
+  static const Color lightCard = Color(0xFFF5F4EF);
+  static const Color lightCardSecondary = Color(0xFFEFEEE9);
+  static const Color lightTextPrimary = Color(0xFF1B1C19);
+  static const Color lightTextSecondary = Color(0xFF727971);
+  static const Color lightTextMuted = Color(0xFF9E9E96);
+  static const Color lightBorder = Color(0xFFE3E3DE);
+  static const Color lightDivider = Color(0xFFE9E8E3);
+
+  // Dark Theme Colors (الوضع السَّكِينِي الليلي)
+  static const Color darkBackground = Color(0xFF141E18);
+  static const Color darkSurface = Color(0xFF1B2A22);
+  static const Color darkCard = Color(0xFF1F3027);
+  static const Color darkCardSecondary = Color(0xFF25392F);
+  static const Color darkTextPrimary = Color(0xFFFAF9F4);
+  static const Color darkTextSecondary = Color(0xFFC2C8BF);
+  static const Color darkTextMuted = Color(0xFF7F8A7D);
+  static const Color darkBorder = Color(0xFF283B30);
+  static const Color darkDivider = Color(0xFF223329);
+
+  // Functional Status Colors
+  static const Color success = Color(0xFF2E7D32);
+  static const Color warning = Color(0xFFED6C02);
+  static const Color error = Color(0xFFD32F2F);
+  static const Color info = Color(0xFF0288D1);
+}
